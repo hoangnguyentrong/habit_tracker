67945a95b3690c5894985e13f067289cf5e2d35f
Nguyễn Trọng Hoàng <hoangnguyen30092003@gmail.com>
2024-05-20 22:04:26 +0700

    -delete

--- final_test/routers/user.js
+++
@@ -1,8 +0,0 @@
-
-const userController = require("../controller/userController");
-
-const router = require("express").Router();
-
-router.post("/", userController.signupUser);
-
-module.exports = router;
